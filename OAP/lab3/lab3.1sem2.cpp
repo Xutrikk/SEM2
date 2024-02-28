55f08588bd1b40ae5786cd9721f026a37949ebe6
Kirill <153691801+Xutrikk@users.noreply.github.com>
2024-02-29 01:50:06 +0300

    Add files via upload

---
+++ OAP/lab3/lab3.1sem2.cpp
@@ -0,0 +1,70 @@
+﻿#include <fstream>
+#include <iostream>
+#include <stdio.h>
+
+// Ввести с клавиатуры строку символов, состоящую из слов, разделенных пробелами, и записать ее в файл.
+// Прочитать из файла данные и вывести все слова, которые содержат букву «х».
+
+// Функция для записи введенной строки в файл
+void fromFile(char s[]);
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    char buff[1000];  
+    char str[1000];   
+    int i, j, t;
+
+    cout << "Введите строку : ";
+    gets_s(str);  // Считываем входную строку от пользователя
+
+    fromFile(str);  // Записываем входную строку 
+
+    cout << endl;
+
+    t = 0;
+    j = 0;
+    ifstream fin("f1.txt");  
+    if (!fin.is_open())
+        cout << "Файл не может быть открыт!\n"; 
+    else
+    {
+        while (!fin.eof())  // Читаем слова из файла до конца файла
+        {
+            fin >> buff;  // Читаем слово из файла
+            j = j + 1;
+            for (i = 0; i < strlen(buff); i++)
+            {
+                if (buff[i] == 'x')  
+                {
+                    cout << buff << endl; 
+                    break;
+                }
+                else
+                    t = t + 1;
+            }
+        }
+        if (t == j)
+            cout << " Слов с буквой x нет";  
+    }
+    fin.close();  
+
+    return 0;
+}
+
+
+void fromFile(char s[50])
+{
+    ofstream f("f1.txt");  
+
+    if (f.fail())
+    {
+        cout << "\n Ошибка открытия файла";  
+        exit(1);
+    }
+    else
+        f << s;  
+    f.close();  
+}
---
+++ OAP/lab3/lab3sem2.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream> 
+#include <fstream> 
+#include <sstream> 
+#include <map> 
+//Скопировать из файла FILE1 в файл FILE2 все строки, в которых содержится не менее двух одинаковых слов. 
+//Определить номер слова, в котором больше всего цифр.
+
+using namespace std; 
+
+// Функция для копирования строк с двумя или более одинаковыми словами
+void copyLinesWithDuplicateWords(const string& inputFile, const string& outputFile) {
+    setlocale(LC_ALL, "rus"); 
+    ifstream inFile(inputFile); 
+    ofstream outFile(outputFile); 
+    string line; 
+
+    // Читаем строки из файла
+    while (getline(inFile, line)) {
+        istringstream iss(line); // создаем строковый поток
+        map<string, int> wordCount; // map для подсчета слов
+        string word; 
+
+        // Читаем слова из строки
+        while (iss >> word) {
+            wordCount[word]++; 
+        }
+
+        // Проверяем количество одинаковых слов
+        for (auto& wc : wordCount) {
+            if (wc.second >= 2) {
+                outFile << line << '\n'; 
+                break;
+            }
+        }
+    }
+
+    inFile.close(); 
+    outFile.close(); 
+}
+
+// Функция для определения номера слова с наибольшим количеством цифр
+int wordWithMostDigits(const string& inputFile) {
+    ifstream inFile(inputFile); 
+    string line; 
+    int maxDigits = 0, wordNumber = 0, maxDigitsWordNumber = 0; 
+
+    // Читаем строки из файла
+    while (getline(inFile, line)) {
+        istringstream iss(line); // создаем строковый поток
+        string word; 
+
+        // Читаем слова из строки
+        while (iss >> word) {
+            wordNumber++; 
+            int digitCount = 0; 
+
+            // Подсчитываем количество цифр в слове
+            for (char c : word) {
+                if (isdigit(c)) digitCount++;
+            }
+
+            // Если количество цифр больше максимального, обновляем максимальное количество цифр и номер слова
+            if (digitCount > maxDigits) {
+                maxDigits = digitCount;
+                maxDigitsWordNumber = wordNumber;
+            }
+        }
+    }
+
+    inFile.close(); 
+    return maxDigitsWordNumber; 
+}
+
+int main() {
+    string inputFile = "FILE1.txt"; 
+    string outputFile = "FILE2.txt"; 
+
+    copyLinesWithDuplicateWords(inputFile, outputFile); 
+    cout << "Слово с наибольшим количеством цифр: " << wordWithMostDigits(inputFile) << endl; 
+
+    return 0; 
+}
