9de141359dbdc843070059139333c65cf3b6ca49
Kirill <153691801+Xutrikk@users.noreply.github.com>
2024-02-29 01:51:15 +0300

    Add files via upload

---
+++ OAP/lab4/lab4dop1.cpp
@@ -0,0 +1,85 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+//Определить структуру для представления информации о сданных студентом экзаменах, содержащую поля: 
+// ФИО студента, число экзаменов, полученные оценки.
+//Определить функции для обработки отдельного объекта (например, для проверки, сданы ли все экзамены на 4 и 5). 
+//Написать функцию для обработки массива структур.
+//В результате обработки требуется вычислить характеристику успеваемости студентов,
+//то есть отношение числа студентов, сдавших экзамены на 4 и 5, к общему числу студентов, в процентах.
+
+// Определение структуры для представления информации о сданных студентом экзаменах
+struct student {
+	string fio; 
+	int num_ex; 
+	int* marks; 
+};
+
+// Функция для проверки, сданы ли все экзамены на 4 и 5
+bool zachet(student a);
+
+// Функция для вычисления характеристики успеваемости студентов
+double prosent(student* a, int n);
+
+void main() {
+	setlocale(LC_ALL, "rus");
+	student a[10]; 
+	cout << "Введите количесвто студентов" << endl;
+	int n; 
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cout << "fio" << endl;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		getline(cin, a[i].fio); 
+		cout << "Количество экзаменов" << endl;
+		cin >> a[i].num_ex; 
+		cout << "Оценки" << endl;
+		a[i].marks = new int[a[i].num_ex]; // Выделение памяти под оценки
+		for (int j = 0; j < a[i].num_ex; j++) {
+			cin >> a[i].marks[j]; 
+		}
+	}
+	int q; 
+	cout << "Введите номер студента которого нужно проверить:" << endl;
+	cin >> q;
+	if (zachet(a[q-1]) == true) {
+		cout << "Данный студент успешно закрыл сессию" << endl; 
+	}
+	else {
+		cout << "Данный студент не сдал сессию" << endl; // Вывод сообщения, если студент не закрыл сессию
+	}
+	cout << "Процентное отношение отличников к общему количеству студентов:" << endl;
+	cout << prosent(a, n); 
+	cout << " процентов";
+}
+
+// Функция для проверки, сданы ли все экзамены на 4 и 5
+bool zachet(student a) {
+	int count = a.num_ex; 
+	for (int j = 0; j < count; j++) {
+		if (a.marks[j] < 4) 
+			return false;		
+	}
+	return true; // Возвращаем true, если все экзамены сданы на 4 и 5
+}
+
+// Функция для вычисления характеристики успеваемости студентов
+double prosent(student* a, int n) {
+	double good = 0; 
+	int all = 0; 
+	for (int i = 0; i < n; i++) {
+		bool flag = true; 
+		int count = a[i].num_ex; 
+		for (int j = 0; j < count; j++) {
+			if (a[i].marks[j] < 4) { 
+				flag = false; 
+				break; 
+			}
+		}
+		if (flag == true) { 
+			good++; 
+		}
+	}
+	double pros = (good / n) * 100; 
+	return pros; 
+}
---
+++ OAP/lab4/lab4dop2.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+//Описать структуру с именем TRAIN, содержащую поля: названия пункта назначения, номер поезда, время отправления.
+//Написать программу, выполняющую ввод с клавиатуры данных в массив, состоящий из восьми элементов типа TRAIN
+//(записи должны быть размещены в алфавитном порядке по названиям пунктов назначения); 
+//вывод на экран информации о поездах, отправляющихся после введенного с клавиатуры времени 
+//(если таких поездов нет, то вывести сообщение об этом).
+using namespace std;
+
+// Определение структуры TRAIN, которая содержит информацию о поезде
+struct TRAIN {
+    string destination; 
+    int number; 
+    string departure_time;
+};
+
+// Функция для сравнения двух поездов по пункту назначения
+bool compareByDestination(const TRAIN& a, const TRAIN& b) {
+    return a.destination < b.destination; // Возвращает true, если пункт назначения поезда a предшествует пункту назначения поезда b
+}
+
+// Функция для сортировки массива поездов по пункту назначения
+void sortTrains(TRAIN trains[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (compareByDestination(trains[j], trains[j + 1])) {
+                swap(trains[j], trains[j + 1]); // Меняем местами поезда, если пункт назначения поезда j предшествует пункту назначения поезда j+1
+            }
+        }
+    }
+}
+
+
+int main() {
+    setlocale(LC_ALL, "Russian"); 
+    const int SIZE = 8; 
+    TRAIN trains[SIZE]; // Создание массива из 8 элементов типа TRAIN
+
+    // Ввод данных о поездах с клавиатуры
+    for (int i = 0; i < SIZE; i++) {
+        cout << "Введите пункт назначения, номер поезда и время отправления для поезда " << i + 1 << ": ";
+        cin >> trains[i].destination >> trains[i].number >> trains[i].departure_time;
+    }
+
+    // Сортировка поездов по пункту назначения в алфавитном порядке
+    sortTrains(trains, SIZE);
+
+    string input_time; 
+    cout << "Введите время: ";
+    cin >> input_time;
+
+    bool found = false; // Флаг, указывающий, найдены ли поезды, отправляющиеся после введенного времени
+
+    // Поиск поездов, отправляющихся после введенного времени
+    for (int i = 0; i < SIZE; i++) {
+        if (trains[i].departure_time > input_time) {
+            found = true;
+            cout << "Поезд номер " << trains[i].number << " отправляется в " << trains[i].departure_time << " в направлении " << trains[i].destination << ".\n";
+        }
+    }
+
+    
+    if (!found) {
+        cout << "Поездов, отправляющихся после введенного времени, не найдено.\n";
+    }
+
+    return 0; 
+}
