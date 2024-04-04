80d986360d081840eb57ffbdfc8570686fa27e1a
Kirill <153691801+Xutrikk@users.noreply.github.com>
2024-04-05 00:35:41 +0300

    Add files via upload

---
+++ OAP/lab10/lab10oap.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int S(int x) {
+    if (x > 100) {
+        return x + 10;
+    }
+    else {
+        return S(x + 4);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int x;
+    cout << "Введите x: ";
+    cin >> x;
+    cout << "S(x) = " << S(x) << endl;
+    return 0;
+}
