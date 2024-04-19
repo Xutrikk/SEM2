0693c0a1851e62f9dcdc37fcc48c4cfb6991058d
Kirill <153691801+Xutrikk@users.noreply.github.com>
2024-04-19 22:11:19 +0300

    Add files via upload

---
+++ AISD/lab4/lab4.1aisd.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <ctime> 
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	srand(time(NULL));
+
+	int N, tmp, step;
+	int winners = 0; 
+	int i, j, k;
+
+	do
+	{
+		cout << "Введите количество участников: ";
+		cin >> N;
+
+		if (N > 9999 || N <= 0) {
+			cout << "Зачем это вообще проверять" << endl;
+		}
+
+	} while (N > 9999 || N <= 0);
+
+	if (N < 3)
+	{
+		cout << "Количество призёров = " << N;
+		return 0;
+	}
+
+	int size = N;
+
+	int* participantScores = new int[size]; 
+
+	for (i = 0; i < size; i++)	
+		participantScores[i] = (rand() % 7 + 1);
+
+
+	// Сортировка выбором
+	for (int i = 0; i < N - 1; i++) {
+		int min = i;
+		for (int j = i + 1; j < N; j++) {
+			if (participantScores[j] < participantScores[min]) {
+				min = j;
+			}
+		}
+		if (min != i) {
+			swap(participantScores[i], participantScores[min]);
+		}
+	}
+
+
+	unsigned int top1 = participantScores[size - 1], top2 = participantScores[size - 1], top3 = participantScores[size - 1];
+	bool top2Check = true, top3Check = true; 
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		if (top2 > participantScores[i] && top2Check && top2 >= top1)
+		{
+			top2 = participantScores[i];
+			top2Check = false;
+		}
+		if (top3 > participantScores[i] && top3Check && participantScores[i] != top2)
+		{
+			top3 = participantScores[i];
+			top3Check = false;
+		}
+
+
+	}
+
+	// Цикл посчёта количества победителей 
+
+	for (i = size - 1; i >= 0; i--)
+		if (participantScores[i] == top1 || participantScores[i] == top2 || participantScores[i] == top3) {
+			winners++;
+		}
+
+	cout << endl << endl;
+
+	cout << "Начальные данные: " << endl;
+	for (i = 0; i < size; i++) {
+		cout << participantScores[i] << ' ';
+	}
+
+	cout << endl << endl;
+
+	cout << top1 << ' ' << top2 << ' ' << top3;
+
+
+	cout << endl << endl;
+
+	cout << "Количество призёров = " << winners << endl << endl;
+
+	return 0;
+}
---
+++ AISD/lab4/lab4aisd.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using namespace std;
+void printArray(int* arr, int size) {
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+void Sort(int* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    int* tempArr = new int[size];
+    int minIndex = 0;
+    for (int i = 0; i < size; i++) {
+        if (i % 2 == 0) {
+            tempArr[i] = arr[size - minIndex - 1];
+        }
+        else {
+            tempArr[i] = arr[minIndex++];
+        }
+    }
+    for (int i = 0; i < size; i++) {
+        arr[i] = tempArr[i];
+    }
+
+    delete[] tempArr;
+}
+int summa(int* arr, int size) {
+    int sum = 0;
+    for (int i= 0; i < size; i++) {
+        if (i % 2 != 0) { 
+            sum += arr[i - 1];
+        }
+    }
+    return sum;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+	int kol;
+	cout << "Введите количество товаров:\n";
+	cin >> kol;
+	if (kol > 9999) {
+		cout << "Зачем это вообще проверять" << endl;
+		return 0;
+	}
+	cout << "Изначальный порядок:"<< endl;
+	int* arr = new int[kol];
+	for (int i = 0; i < kol; i++) {
+		arr[i] = rand() % 99 + 1;
+		cout << arr[i] << " ";
+	}
+    cout << "\nХитрый кассир намутил:" << endl;
+	Sort(arr, kol);
+    printArray(arr, kol);
+    int MaxCheck=summa(arr, kol);
+    cout << "\nМаксимальная сумма чека:" << MaxCheck << endl;
+	return 0;
+}
